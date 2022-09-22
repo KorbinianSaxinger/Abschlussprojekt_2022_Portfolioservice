3c8cc38715f0d2c577ce14548ae5af4c3ab40d1c
Korbinian Saxinger <k.saxinger@webconia.de>
2022-09-22 21:02:58 +0200

    xyx

--- abschlusspruefung_korbinian_saxinger/src/App.vue
+++ abschlusspruefung_korbinian_saxinger/src/App.vue
@@ -1,25 +1,23 @@
 <template>
-  <main-app msg="Abschlussprojekt Portfolio Service"/>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
-<script>
 
-import MainApp from "@/components/Main-App";
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
   components: {
-    MainApp
-  }
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
