af66331f7fc633d52e7c247f6eea7a8cce86ef9e
Korbinian Saxinger <k.saxinger@webconia.de>
2022-09-22 19:18:00 +0200

    Create Main-App

--- abschlusspruefung_korbinian_saxinger/src/App.vue
+++ abschlusspruefung_korbinian_saxinger/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main-app msg="Abschlussprojekt Portfolio Service"/>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MainApp from "@/components/Main-App";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainApp
   }
 }
 </script>
