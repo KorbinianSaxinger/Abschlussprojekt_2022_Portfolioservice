b2df0153ee63df3bec11320840eea8d1c4d5508a
Korbinian Saxinger <k.saxinger@webconia.de>
2022-09-23 15:31:42 +0200

    Create header
    finished header skeleton

---
+++ abschluss_projekt/src/components/dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <app-header/>
+    </div>
+</template>
+
+<script>
+  import AppHeader from "@/components/header";
+  export default {
+    name: 'dash-board',
+    components: {
+      AppHeader,
+    },
+
+    data: () => ({
+
+    }),
+  }
+</script>
---
+++ abschluss_projekt/src/components/header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header d-flex">
+      <v-img
+          class="logo align-left"
+          max-height="100"
+          max-width="100"
+          src="../assets/logo.png"
+      ></v-img>
+          <p class="text-md-center">Portfolio Service Korbinian Saxinger</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app-header',
+
+    data: () => ({
+
+    }),
+  }
+</script>
+
+<style scoped>
+
+.header {
+    height: 110px;
+    max-height: 110px;
+}
+
+p {
+  position: inherit;
+  margin-left: 37%;
+  margin-top: 50px;
+  color: black;
+}
+
+.logo {
+  margin: 10px 10px 10px 10px;
+}
+</style>
