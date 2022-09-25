b5a8d0f0b735ccd6896e875b86a6cf22ff407437
KorbinianSaxinger <97603081+KorbinianSaxinger@users.noreply.github.com>
2022-09-25 14:27:21 +0200

    Update Home.vue

--- abschluss_projekt/src/components/Home.vue
+++ abschluss_projekt/src/components/Home.vue
@@ -4,6 +4,13 @@
       v-if="user !== ''"
     >
       <h1>Home</h1>
+      <v-textarea
+          v-model="text"
+      ></v-textarea>
+      <v-btn class="btn" @click.prevent="fetchData">Lade Daten</v-btn>
+      <v-list v-for="item in todo_list" :key="item.id">
+          <v-list-item> {{ item }}</v-list-item>
+      </v-list>
     </div>
     <div
         v-if="user === ''"
@@ -18,15 +25,24 @@
 import {getAuth, onAuthStateChanged} from "firebase/auth";
 import app from "../../firebase";
 import PLogin from "@/components/auth/login";
+import axios from "vue-axios";
 
 export default {
   name: "PHome",
   components: {PLogin},
   data() {
     return {
+      todo_neu: "",
+      todo_list: [],
+      text: 'test',
       user: '',
       email: '',
       passwort: '',
+    }
+  },
+  methods: {
+    fetchData() {
+      axios.get('http://127.0.0.1/get.php').then(response => (this.todo_list = response.data));
     }
   },
   mounted() {
